90814134661012e692b0415c6c5bef43ccfc6a6b
npty <nptytn2@gmail.com>
2024-11-27 17:06:05 +0700

    chore: remove unused function in abi

--- abi/interchain-token-factory.ts
+++ abi/interchain-token-factory.ts
@@ -12,7 +12,6 @@ export const InterchainTokenFactoryABI = [
 
   // Write functions
   "function deployInterchainToken(bytes32 salt, string name, string symbol, uint8 decimals, uint256 initialSupply, address minter) payable returns (bytes32 tokenId)",
-  "function deployRemoteInterchainToken(bytes32 salt, address minter, string destinationChain, uint256 gasValue) payable returns (bytes32 tokenId)",
   "function deployRemoteInterchainToken(string originalChainName, bytes32 salt, address minter, string destinationChain, uint256 gasValue) payable returns (bytes32 tokenId)",
   "function registerCanonicalInterchainToken(address tokenAddress) payable returns (bytes32 tokenId)",
   "function deployRemoteCanonicalInterchainToken(address originalTokenAddress, string destinationChain, uint256 gasValue) payable returns (bytes32 tokenId)",
